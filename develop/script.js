e807268b7ffd53cb4ad57ba9256ecf2d220553f8
Samantha Leyva <samanthaleyva@Samanthas-MacBook-Air-2.local>
2022-11-20 17:50:20 -0600

    finished checkAnswer function, started clearPrevious function

--- develop/script.js
+++ develop/script.js
@@ -36,15 +36,18 @@ let questions = [
     answer: "console.log",
   },
 ];
+let numberOfQuestions = questions.length - 1; // -1 to account for the index starting at 0
 let currentQuestionIndex = 0;
+let score = 0;
+let userChoice = "";
 
 // functions
 function beginQuiz() {
   questionsBox.classList.remove("hidden");
-  createQuestion(currentQuestionIndex);
+  displayQuestion(currentQuestionIndex);
 }
 
-function createQuestion(index) {
+function displayQuestion(index) {
   let questionTitle = document.createElement("h2");
   questionTitle.textContent = questions[index].title;
   questionsDiv.append(questionTitle);
@@ -52,14 +55,44 @@ function createQuestion(index) {
     let choiceBtn = document.createElement("button");
     choiceBtn.textContent = choice;
     questionsDiv.append(choiceBtn);
+    choiceBtn.addEventListener("click", checkAnswer);
   });
 }
+
+function checkAnswer() {
+  // Check if answer is correct/incorrect, adds points to score, and shows next question
+  if (currentQuestionIndex > numberOfQuestions) {
+    console.log("game over");
+    // add function that ends the game - scoreboard
+  } else if (
+    this.textContent === questions[currentQuestionIndex].answer &&
+    currentQuestionIndex === numberOfQuestions
+  ) {
+    currentQuestionIndex++;
+    score++;
+    correctOrIncorrect.textContent = "Correct";
+    console.log("game over");
+    // add function that ends the game - scoreboard
+  } else if (
+    this.textContent === questions[currentQuestionIndex].answer &&
+    currentQuestionIndex < numberOfQuestions
+  ) {
+    currentQuestionIndex++;
+    score++;
+    correctOrIncorrect.textContent = "Correct";
+    displayQuestion(currentQuestionIndex);
+  } else {
+    correctOrIncorrect.textContent = "Incorrect";
+  }
+}
+
+//clearPrevious function
+
 
 // event listeners
 // init - when the person comes to the page
 // start quiz button
 startQuizBtn.addEventListener("click", beginQuiz);
-// select an answer buttons?
 
 // this function will call the next question in the array
 // input your name and score
